1e2853522e1f6ffc2fdd162ccb4e480e60b4b542
Handsken <nan.222@yandex.ru>
2023-05-05 13:41:58 +0500

    first

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/controller/EmployeeController.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.lessons.springboot.weblibrary.Employee;
 import ru.skypro.lessons.springboot.weblibrary.service.EmployeeService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,9 +19,55 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    @ResponseBody
     @GetMapping
     public List<Employee> showCounter() {
         return employeeService.getAllEmployees();
+    }
+    @GetMapping("/salary/sum")
+    public String showSumSalary(){
+        List<Employee> a = employeeService.getAllEmployees();
+        int salary = 0;
+        for (int i = 0; i < a.size() ; i++) {
+            salary+= a.get(i).getSalary();
+        }
+        return "Сумма зарплат всех сотрудников: "+ salary;
+    }
+    @GetMapping("/salary/min")
+    public String showMinSalary(){
+        List<Employee> a = employeeService.getAllEmployees();
+        int salary=a.get(0).getSalary();
+        for (int i = 0; i < a.size(); i++) {
+            if(salary>a.get(i).getSalary()){
+                salary=a.get(i).getSalary();
+            }
+        }
+        return "Минимальная зарплата среди сотрудников: "+salary;
+    }
+    @GetMapping("/salary/max")
+    public String showMaxSalary(){
+        List<Employee> a = employeeService.getAllEmployees();
+        int salary=a.get(0).getSalary();
+        for (int i = 0; i < a.size(); i++) {
+            if (salary<a.get(i).getSalary()){
+                salary=a.get(i).getSalary();
+            }
+        }
+        return "Наибольшая зарплата среди сотрудников: "+salary;
+    }
+    @GetMapping("/high-salary")
+    public String showHighSalary(){
+        List<Employee> a = employeeService.getAllEmployees();
+        List<Employee> b = new ArrayList<>();
+        int count=0;
+        for (int i = 0; i < a.size(); i++) {
+            count+=a.get(i).getSalary();
+        }
+        count=count/a.size();
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i).getSalary()>count){
+                b.add(a.get(i));
+            }
+        }
+        return "Сотрудники, зарплата которых больше средней: "+b;
     }
 }
